c14a9cf5a6bbe7bd6a793fdd99277bce93f8012d
larissabndct <larissagomes0119@gmail.com>
2024-10-03 21:59:51 -0300

    testes

--- JS/aula03/index.js
+++ JS/aula03/index.js
@@ -1,30 +1,17 @@
-// console.log("Olá mundo!");
-// /* 
-// 1 - NUNCA iniciar com números ou caracteres especiais. Exceto o _ (underline/underscore)
-// 2 - Nunca utilizar espaço para separar uma palavra conjunta.
-//     Ex: variavel nome pessoa (errado) | variavel nome_pessoa | nomePessoa | NomePessoa
-// 3 - Sempre utilizar ou var, const ou let:
-//     var   - variável que pode sofrer ou não alteração.
-//     let   - variável que sofrerá alteração.
-//     const - constante, seu valor nunca mudará.
-// */
+let numero = 8;
+console.log(`0 x 8 = ${0*numero}`);
+console.log(`1 x 8 = ${1*numero}`);
+console.log(`2 x 8 = ${2*numero}`);
+console.log(`3 x 8 = ${3*numero}`);
+console.log(`4 x 8 = ${4*numero}`);
+console.log(`5 x 8 = ${5*numero}`);
+console.log(`6 x 8 = ${6*numero}`);
+console.log(`7 x 8 = ${7*numero}`);
+console.log(`8 x 8 = ${9*numero}`);
+console.log(`9 x 8 = ${9*numero}`);
+console.log(`10 x 8 = ${10*numero}`);
 
-// /*
-// snake_case - Separado por underline/underscore (_)
-// PascalCase - Separar as palavras por caracter maiúsculo
-// camelCase  - Separar as palavras iniciando por caractere maiúsculo a partir da segunda palavra.
-// */
-// const nome = "Larissa";
-// console.log(nome);
-
-/**
- * Criem 3 variáveis para nome, idade e cidade.
- * Criem uma frase com as 3 variáveis.
- * Imprima no console
- */
-
-let nome = "Larissa";
-let idade = 23;
-let cidade = "Araraquara";
-
-console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`);
+for(var i = 0; i <=10; i++)
+{
+    console.log(` ${i} x 8 = ${i*8}`);
+}
