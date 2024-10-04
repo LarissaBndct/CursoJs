83c40cf238277d984a9c2594625ce69fdcaa00c9
larissabndct <larissagomes0119@gmail.com>
2024-10-03 21:00:23 -0300

    teste

---
+++ JS/aula03/index.js
@@ -0,0 +1,30 @@
+// console.log("Olá mundo!");
+// /* 
+// 1 - NUNCA iniciar com números ou caracteres especiais. Exceto o _ (underline/underscore)
+// 2 - Nunca utilizar espaço para separar uma palavra conjunta.
+//     Ex: variavel nome pessoa (errado) | variavel nome_pessoa | nomePessoa | NomePessoa
+// 3 - Sempre utilizar ou var, const ou let:
+//     var   - variável que pode sofrer ou não alteração.
+//     let   - variável que sofrerá alteração.
+//     const - constante, seu valor nunca mudará.
+// */
+
+// /*
+// snake_case - Separado por underline/underscore (_)
+// PascalCase - Separar as palavras por caracter maiúsculo
+// camelCase  - Separar as palavras iniciando por caractere maiúsculo a partir da segunda palavra.
+// */
+// const nome = "Larissa";
+// console.log(nome);
+
+/**
+ * Criem 3 variáveis para nome, idade e cidade.
+ * Criem uma frase com as 3 variáveis.
+ * Imprima no console
+ */
+
+let nome = "Larissa";
+let idade = 23;
+let cidade = "Araraquara";
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`);
