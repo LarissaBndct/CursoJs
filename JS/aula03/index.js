d701fa5a6453a114a9cca0cdc0a44572fd3e29af
LarissaBndct <larissagomes0119@gmail.com>
2024-10-08 20:22:31 -0300

    readline com conversao

--- JS/aula03/index.js
+++ JS/aula03/index.js
@@ -1,24 +1,24 @@
-let numero = 8;
-console.log(`0 x 8 = ${0*numero}`);
-console.log(`1 x 8 = ${1*numero}`);
-console.log(`2 x 8 = ${2*numero}`);
-console.log(`3 x 8 = ${3*numero}`);
-console.log(`4 x 8 = ${4*numero}`);
-console.log(`5 x 8 = ${5*numero}`);
-console.log(`6 x 8 = ${6*numero}`);
-console.log(`7 x 8 = ${7*numero}`);
-console.log(`8 x 8 = ${9*numero}`);
-console.log(`9 x 8 = ${9*numero}`);
-console.log(`10 x 8 = ${10*numero}`);
+// let numero = 8;
+// console.log(`0 x 8 = ${0*numero}`);
+// console.log(`1 x 8 = ${1*numero}`);
+// console.log(`2 x 8 = ${2*numero}`);
+// console.log(`3 x 8 = ${3*numero}`);
+// console.log(`4 x 8 = ${4*numero}`);
+// console.log(`5 x 8 = ${5*numero}`);
+// console.log(`6 x 8 = ${6*numero}`);
+// console.log(`7 x 8 = ${7*numero}`);
+// console.log(`8 x 8 = ${9*numero}`);
+// console.log(`9 x 8 = ${9*numero}`);
+// console.log(`10 x 8 = ${10*numero}`);
 
-for(var i = 0; i <=10; i++)
-{
-    console.log(` ${i} x 8 = ${i*8}`);
-}
-const readline = require('readline-sync');
+ const readline = require('readline-sync');
 
-const nome = readline.question("Qual o seu nome? ");
-console.log(`O nome informado foi ${nome}`);
+// const nome = readline.question("Qual o seu nome? ");
+// console.log(`O nome informado foi ${nome}`);
+// const idade = parseInt(readline.question("Qual sua idade?")); 
+// console.log(`a idade informada foi ${idade}`);
+
+
 
 // const rl = readline.createInterface({
 //     input: process.stdin,
@@ -30,3 +30,28 @@ console.log(`O nome informado foi ${nome}`);
 //     rl.close();
 // });
 
+var numero = readline.questionFloat("Informe o numero para saber a tabuada: ");
+
+var multiplicador = 0;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
+console.log(` ${numero} x ${multiplicador} = ${numero * multiplicador++ } `);
+multiplicador = multiplicador;
