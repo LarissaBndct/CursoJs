728f05b069ca029bfd7f19e59a9e1607a8e60040
larissabndct <larissagomes0119@gmail.com>
2024-10-30 12:12:08 -0300

    ajustes

--- JS/estruturaDecisao.js
+++ JS/estruturaDecisao.js
@@ -1,5 +1,5 @@
-//estruturas de decisão são blocos de código que são utilizados para a quebra de fluxo de um algoritmo.
-//essas estruturas são executadas a partir de uma condição
+/*estruturas de decisão são utilizadas quando é necessária uma mudança no fluxo do código
+de acordo com uma determinada condição */
 //para criar uma estrutura de decisão utiliza-se o if seguido da condição necessária para que ela seja executada.
 //Pode ser acompanhada por um else if ou um else, que não são obrigatórios: 
 
@@ -21,4 +21,6 @@ else
 }
 
 //if ternário é um if de uma linha só. Sua sintaxe é: <condição ? resposta se condição == true : resposta se condição == false;> :
-
+let idade = 18;
+let mensagem = idade >= 18 ? "Você é maior de idade." : "Você é menor de idade.";
+console.log(mensagem); // Imprime: Você é maior de idade.
