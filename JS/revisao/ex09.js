1b4e37e3ba69efb29e6383e30209cd7c8c2fdbe8
larissabndct <larissagomes0119@gmail.com>
2024-10-30 12:33:43 -0300

    termino ex09

--- JS/revisao/ex09.js
+++ JS/revisao/ex09.js
@@ -1,8 +1,17 @@
 // * 9 - Escreva um programa que tenha uma função chamada ehPrimo. Receba um número informado pelo usuário, passe ele para a função. Se o número for primo, deverá retornar true, senão false.
 const readline = require("readline-sync");
-function ehPrimo(num)
-{
-    return null
+function ehPrimo(numero) {
+    if (numero <= 1) return "Não é primo";
+
+    for (let contador = 2; contador <= Math.sqrt(numero); contador++) {
+        if (numero % contador === 0) {
+            return "não é primo";
+        }
+    }
+    return "é primo";
 }
-let numero = readline.questionInt("Informe um numero: ");
-console.log(`o numero ${numero} ${ehPrimo(numero)}`);
+
+let numero = readline.question("Informe um numero:");
+let resultado = ehPrimo(numero);
+
+console.log(`o número ${numero} ${resultado}`);
