4175d18cbe95651d44bedaf091aa0f97914433ca
larissabndct <larissagomes0119@gmail.com>
2024-11-05 21:49:40 -0300

    todo: caminho assets

--- JSII/assets/js/principal.js
+++ JSII/assets/js/principal.js
@@ -11,8 +11,23 @@ $(document).ready(function() {
     });
 
     $("#btnApagar").on('click', function() {
-        alert('O usuário foi apagado')
-        $("#nomeUsuario").val("")
+        // $("#nomeUsuario").val('').focus()
+        // $("#senha").val('')
+        document.getElementById("nomeUsuario").value = ""
+        document.getElementById("senha").value = ""
+        document.getElementById("nomeUsuario").focus()
     });
 
+    $("#btnMensagem").on('click', () => {
+        // var xmlHttp = new XMLHttpRequest();
+        // xmlHttp.open( "GET", "https://catfact.ninja/fact", false ); // false for synchronous request
+        // xmlHttp.send( null );
+
+        // const resposta = JSON.parse(xmlHttp.responseText);
+
+        $.get("https://catfact.ninja/fact", (data) => {
+            $("#frase").text(data.fact)
+        });
+
+    });
 });
